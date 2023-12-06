b9abf04b5cb1cf2960ab7409463432aa9122962a
Mohammed Tajammul <76564671+Tajammul607@users.noreply.github.com>
2023-12-06 16:59:32 +0000

    --

---
+++ practices/01_variables.js
@@ -0,0 +1,8 @@
+//  variable just like a container
+
+const accountId=123;
+let fullName="Mohammed Tajammul";
+var accountEmail="tajammul@google.com"
+accountCity='Bengaluru'
+
+console.table([accountId,fullName,accountEmail,accountCity])
