65f73d00602ad492f309c36c37abd76b258dc5df
Mohammed Tajammul <76564671+Tajammul607@users.noreply.github.com>
2023-12-10 09:45:49 +0000

    couple of date method

---
+++ practices/DataAndTime.js
@@ -0,0 +1,35 @@
+let date=new Date();
+
+// console.log(date);
+
+// console.log(date.toString());
+
+// console.log(date.toLocaleString);//12/10/2023, 2:41:36 PM
+
+// console.log(date.toLocaleDateString)
+
+// console.log(typeof date)
+
+// let specificDate=new Date(2023,7,24);
+
+// let specificDate=new Date(2023 ,7, 24 , 5,6)
+
+let specificDate=new Date("01-08-2019")
+
+// console.log(specificDate.toDateString())
+
+// console.log(specificDate.toLocaleString())
+
+// console.log(specificDate.getTime())
+
+// console.log(Date.now())
+
+// console.log(Math.floor(Date.now()/1000))
+
+let newDate=new Date();
+
+console.log(newDate.getMonth());
+
+console.log(newDate.getDay());
+
+console.log(newDate.getHours());
